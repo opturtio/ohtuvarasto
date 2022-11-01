4aed6b6a8e14d561d03cbdb58645e9382d8dc655
Olli-Pekka Turtio <olli-pekka.turtio@helsinki.fi>
2022-11-01 19:23:18 +0200

    Task 8, 100% coverage

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,35 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+    
+    def test_liikaa_tavaraa(self):
+        self.varasto.lisaa_varastoon(100)
+        
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 0)
+        
+    def test_ottaa_liikaa_tavaraa(self):
+        self.varasto.lisaa_varastoon(7)
+        self.varasto.ota_varastosta(100)
+        
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 10)
+        
+    def test_luokan_tulostus_toimii(self):
+        self.varasto.lisaa_varastoon(6)
+        self.assertAlmostEqual(str(self.varasto), "saldo = 6, vielä tilaa 4")
+        
+    def test_lisataan_varastoon_negatiivinen_luku(self):
+        self.varasto.lisaa_varastoon(-1)
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 10)
+        
+    def test_otetaan_varastosta_negatiivinen_luku(self):
+        self.varasto.ota_varastosta(-1)
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 10)
+    
+    def test_konstruktorille_annetaan_virheellien_tilavuus(self):
+        self.varasto = Varasto(-2)
+        self.assertEqual(self.varasto.tilavuus, 0.0)
+        
+    def test_virheellinen_alkusaldo(self):
+        self.varasto = Varasto(10, -1)
+        self.assertEqual(self.varasto.saldo, 0.0)
+    
