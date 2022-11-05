2c7a997c5db180052d69fca77263105c30992f85
Olli-Pekka Turtio <olli-pekka.turtio@helsinki.fi>
2022-11-05 21:37:56 +0200

    Test class error

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -5,7 +5,7 @@ from varasto import Varasto
 class TestVarasto(unittest.TestCase):
     def setUp(self):
         self.varasto = Varasto(10)
-
+"""
     def test_konstruktori_luo_tyhjan_varaston(self):
         # https://docs.python.org/3/library/unittest.html#unittest.TestCase.assertAlmostEqual
         self.assertAlmostEqual(self.varasto.saldo, 0)
@@ -69,3 +69,4 @@ class TestVarasto(unittest.TestCase):
     def test_virheellinen_alkusaldo(self):
         self.varasto = Varasto(10, -1)
         self.assertEqual(self.varasto.saldo, 0.0)
+"""
